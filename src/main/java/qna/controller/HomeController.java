1d7404a3e6027da9932f951c940143d7862bbbd8
gpfla3690 <gpfla3690@gmail.com>
2022-03-05 17:33:03 +0900

    인덱스 페이지 구현

--- src/main/java/qna/controller/HomeController.java
+++ src/main/java/qna/controller/HomeController.java
@@ -6,33 +6,23 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import qna.service.QuestionService;
 import qna.util.RequestFactory;
 
-@WebServlet("/question/add")
-public class QuestionController extends HttpServlet {
+@WebServlet("/")
+public class HomeController extends HttpServlet {
 	
-	private QuestionService questionSerivce = new QuestionService();
-    
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		RequestFactory factory = new RequestFactory(request, response);
 		
-		factory.forwardToJsp("usr/question/add");
+		factory.forwardToJsp("index");
 		
 	}
 
+
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		RequestFactory factory = new RequestFactory(request, response);
-		
-		HttpServletRequest req = factory.getReq();
-		
-		String title = req.getParameter("title").toString();
-		String body = req.getParameter("body").toString();
-		
-		questionSerivce.save(title, body);
-		
+	
+		doGet(request, response);
 	}
 
 }
---
+++ src/main/webapp/WEB-INF/index.jsp
@@ -0,0 +1,20 @@
+<%@ page language="java" contentType="text/html; charset=EUC-KR"
+    pageEncoding="EUC-KR"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="EUC-KR">
+<title>인덱스</title>
+</head>
+<body>
+
+	<h1>인덱스 입니다.</h1>
+	
+	<div class="link-container">
+		<a href="/question/add">
+			<span>질문하러 가기</span>
+		</a>
+	</div>
+
+</body>
+</html>
